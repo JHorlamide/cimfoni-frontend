b227e59c8a750fd5819588f317bf43c433115de9
JHorlamide <olamidejubril68@gmail.com>
2022-08-26 22:57:36 +0100

    News letter section completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -327,11 +327,11 @@ const Home = () => {
       </div>
 
       {/* OUR STUDENT FEEDBACK */}
-      <div className='bg-studentSectionColor px-10 py-10 lg:px-20 md:px-8 lg:py-10 md:py-10'>
+      <div className='bg-studentSectionColor px-10 py-10 lg:px-20 md:px-8 lg:py-16 md:py-10'>
         <div className='flex flex-col space-y-5'>
           {/* ITEM ONE */}
           <div className='flex flex-col justify-center items-center space-y-3'>
-            <h1 className='text-2xl text-center font-bold text-communityColor'>
+            <h1 className='mx-auto text-2xl text-center font-bold text-communityColor'>
               Our Student Feedbacks
             </h1>
 
