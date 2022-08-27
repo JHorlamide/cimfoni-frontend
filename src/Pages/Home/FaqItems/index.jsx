86afd11a9e5a573dd251a8670da1d1769369a08e
JHorlamide <olamidejubril68@gmail.com>
2022-08-27 15:59:30 +0100

    Fixed hero section

--- src/Pages/Home/FaqItems/index.jsx
+++ src/Pages/Home/FaqItems/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const index = ({ text }) => {
+const index = () => {
   return (
     <div className='flex justify-between border px-2 py-4 rounded-lg space-x-48 lg:space-x-96 md:space-x-96'>
       <p className='text-sm font-normal lg:text-md md:text-md'>asking questions</p>
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -14,24 +14,24 @@ const index = () => {
           Experience that Improves business growth
         </h1>
 
-        <p className='max-w-2xl py-1 px-5 bottom-0 text-md text-justify mx-auto lg:mx-0 md:mx-0 lg:py-5 lg:px-0 md:px-0 md:py-2 lg:max-w-4xl md:max-w-xl'>
+        <p className='max-w-2xl py-1 px-5 bottom-0 text-md text-justify mx-auto lg:mx-0 md:mx-0 lg:py-5 lg:px-0 md:px-0 md:py-2 lg:max-w-5xl md:max-w-xl'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis sit
           gravida mus enim elit egestas ut. Nulla faucibus rutrum phasellus
           iaculis. Donec tristique volutpat id lectus nibh. Lorem commodo, sit
-          elit suspendisse leo, phasellus molestie.
+          elit suspendisse leo, phasellus <br className="hidden lg:block"/>molestie.
         </p>
 
         <div className='flex justify-center items-center lg:justify-start md:justify-start items-start space-x-3 py-3 lg:flex-row'>
           <CustomBtn
             className='text-sm bg-white text-primaryColor font-medium px-4 py-3 rounded-md flex lg:text-md'
-            rightIcon={<ArrowSVG className='w-6 h-6 ml-3' />}
+            rightIcon={<ArrowSVG className='hidden lg:inline-flex md:inline-flex w-6 h-6 ml-3' />}
           >
             View Courses
           </CustomBtn>
 
           <CustomBtn
             className='text-sm border-white bg-primaryColor text-white font-medium px-4 py-3 rounded-md flex lg:border-0 lg:text-md'
-            rightIcon={<ArrowSVG className='w-6 h-6 ml-3' />}
+            rightIcon={<ArrowSVG className='hidden lg:inline-flex md:inline-flex w-6 h-6 ml-3' />}
           >
             Start a project
           </CustomBtn>
